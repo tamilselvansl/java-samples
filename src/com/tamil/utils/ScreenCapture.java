4a19f87c4be8508274dcc5beaea00f15e6603cce
Tamilselvan Teivasekamani <tamil.t@firstpasser.com>
2015-05-28 16:01:58 +0530

    Capture and store present view screen and store it as jpg file

---
+++ src/com/tamil/utils/ScreenCapture.java
@@ -0,0 +1,48 @@
+package com.tamil.apps;
+
+/**
+ * User: Tamil
+ * Date: Nov 14, 2011
+ * Time: 6:38:41 PM
+ * 
+ *  Capture and store present view screen and store it as jpg file
+ */
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ScreenCapture {
+
+    public static void main(String[] args) throws IOException {
+
+        try {
+
+            Robot robot = new Robot();
+			// Creates the delay of 5 sec so that you can open notepad before
+			// Robot start writting
+            robot.delay(5000);
+            robot.keyPress(KeyEvent.VK_H);
+            robot.keyPress(KeyEvent.VK_I);
+            robot.keyPress(KeyEvent.VK_SPACE);
+            robot.keyPress(KeyEvent.VK_B);
+            robot.keyPress(KeyEvent.VK_U);
+            robot.keyPress(KeyEvent.VK_D);
+            robot.keyPress(KeyEvent.VK_D);
+            robot.keyPress(KeyEvent.VK_Y);
+            Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+            Rectangle rect = new Rectangle(0,0,dim.width,dim.height);
+            BufferedImage bi = robot.createScreenCapture(rect);
+            bi.flush();
+            FileOutputStream fi = new FileOutputStream(new File("D:\\screencapture.jpg"));
+            ImageIO.write(bi, "jpg", fi);
+
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+    }
+}
