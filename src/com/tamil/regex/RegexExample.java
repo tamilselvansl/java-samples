bb3008fb240d625d3d049e030fbb0d44b4a082f6
Tamilselvan Teivasekamani <tamil.t@firstpasser.com>
2015-06-03 18:50:48 +0530

    A Regex example to find special character in given string

---
+++ src/com/tamil/regex/RegexExample.java
@@ -0,0 +1,29 @@
+package com.tamil.regex;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.regex.MatchResult;
+
+
+public class RegexExample {
+    
+	public static void main(String[] args) {
+        String regex = "[\\\\!\"#$%&()*+,./:;<=>?@\\[\\]^{|}~]+";
+        String realStrings[] = new String[] {"user-name-@test^"};
+        Pattern p = Pattern.compile(regex);
+
+        for (String realString : realStrings) {
+            Matcher m = p.matcher(realString);
+            boolean splCharFound = m.find();
+            if (splCharFound) {
+                MatchResult result = m.toMatchResult();
+                System.out.println(realString + " has a special character, that is [" + realString.charAt(result.start()) + "]");
+                System.out.println("result.group() = " + result.group());
+                System.out.println("result.groupCount() = " + result.groupCount());
+
+            } else {
+                System.out.println(realString + " not have any special character");
+            }
+        }
+    }
+}
