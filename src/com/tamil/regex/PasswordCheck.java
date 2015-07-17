d5b6135555b8c4ad75d5f3228a3bdc0c9f8fe246
Tamilselvan Teivasekamani <tamil.t@firstpasser.com>
2015-07-17 16:02:11 +0530

    To check a password that should contains a digit, a lowercase, a upper case, one special char from @#$%^&+= and limited to 7 to 15 chars.

---
+++ src/com/tamil/regex/PasswordCheck.java
@@ -0,0 +1,14 @@
+package com.tamil.regex;
+
+public class PasswordCheck {
+
+    public static void main(String[] args) {
+        String passwd = "TestMypass123$.";
+        String pattern = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{7,15}";
+        if (passwd.matches(pattern)) {
+            System.out.println("Password Matches");
+        } else {
+            System.out.println("Password not matches, should contains a one digit, a lowercase, a upper case, one special char in @#$%^&+=, limit to 7 to 15 chars. ");
+        }
+    }
+}
