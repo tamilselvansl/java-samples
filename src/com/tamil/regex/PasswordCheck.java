8a317f625ee6a1166f3951d5e301b099af20e682
Tamilselvan Teivasekamani <tamil.t@firstpasser.com>
2015-07-17 16:07:22 +0530

    To check a password that should contains a digit, a lowercase, a upper case, one special char from @#$%^&+= and limited to 7 to 15 chars.

--- src/com/tamil/regex/PasswordCheck.java
+++ src/com/tamil/regex/PasswordCheck.java
@@ -1,5 +1,13 @@
 package com.tamil.regex;
 
+/**
+ *
+ * Author : Tamilselvan Teivasekamani
+ *
+ * Description : Class to check password with  a digit, a lowercase, a upper case, one special char from @#$%^&+= and limited to 7 to 15 chars.
+ *
+ */
+
 public class PasswordCheck {
 
     public static void main(String[] args) {
